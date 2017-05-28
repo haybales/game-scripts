53972ed94807dfd7f19f9eab41fce71d964fd16c
Sam Furr <samneillfurr@gmail.com>
2017-05-28 15:22:08 -0700

    Added time control test
    Added script to read arduino serial data to control the latitude for the sky system.

---
+++ TimeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class TimeController : MonoBehaviour {
+
+    Tenkoku.Core.TenkokuModule tenkokuModule;
+    SerialPort sp = new SerialPort("COM3", 9600);
+
+    void Start()
+    {
+        tenkokuModule = GameObject.Find("Tenkoku DynamicSky").gameObject.GetComponent<Tenkoku.Core.TenkokuModule>();
+        sp.Open();
+    }
+
+    void Update ()
+    {
+        float val = (float.Parse(sp.ReadLine())/100f)*90f;
+        tenkokuModule.setLatitude = val;
+
+
+	}
+}
